ff52fce623e4e53140302e341e7ec22826ca0ca9
Sahil Sawant <sawantsahil183@email.com>
2026-02-13 10:39:50 +0530

    updated module config setting file

--- saas_core/saas_core/doctype/billing_account_master/test_billing_account_master.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, sahil and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestBillingAccountMaster(FrappeTestCase):
-	pass
